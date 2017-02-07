8e4c832811e7b21ae8deb7806966bc19f9aa428b
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-07 03:27:48 +0200

    Rename the Service Provider

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -5,7 +5,7 @@ namespace Content\Providers;
 use Nova\Module\Support\Providers\ModuleServiceProvider as ServiceProvider;
 
 
-class ContentServiceProvider extends ServiceProvider
+class ModuleServiceProvider extends ServiceProvider
 {
     /**
      * The additional provider class names.
